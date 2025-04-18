93efbcc5831dbcf61f853c4074a34df261e60242
Oleh <iivasiukoleh@gmail.com>
2025-04-19 02:13:05 +0300

    [client]: feat: Attempt #1 to configure Allowed Origins

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/v1";
+const API_URL = import.meta.env.VITE_API_URL || "https://memefy-api.up.railway.app/api/v1";
 
 export { API_URL };
